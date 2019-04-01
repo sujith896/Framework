585d8c698fafa15d9dd49080435d3110d20b6740
sujith, reddy <sujith.talamanchi@gmail.com>
2019-04-02 02:25:25 +0530

    my first commit

---
+++ SeleniumMaven/src/main/java/ibm/SeleniumMaven/App.java
@@ -0,0 +1,13 @@
+package ibm.SeleniumMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SeleniumMaven/src/test/java/samplepackage/SampleOne.java
@@ -0,0 +1,12 @@
+package samplepackage;
+
+public class SampleOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		
+	}
+
+}
